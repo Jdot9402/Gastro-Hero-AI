38a0d09a57c4832b90def74f126d5857d58fd16d
john Edgren <johnedgren@1a:1f:4a:3f:e3:40.home>
2025-10-12 00:35:10 +0200

    Add debug logs to search and API functions

--- api.js
+++ api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30eecfa3cda5c245048242162414551bf0539ef76250279be8080c32798d0ccc
-size 3191
+oid sha256:665b6e640de91b5648117543e8fcae9428d9fdfbbff6384b885c4c117507ae73
+size 3696
