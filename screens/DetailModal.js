73c5a705071c364456f6939d48d2e533a5abaac0
john Edgren <johnedgren@1a:1f:4a:3f:e3:40.home>
2025-10-12 10:03:44 +0200

    Fix SafeAreaView redeclaration and useStrings order in screens

--- screens/DetailModal.js
+++ screens/DetailModal.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229456fd20814c757cd38dfb8bb49eec54ac69b00a7046ec96c9ec9760918864
-size 5782
+oid sha256:52a04541e17be303168e7dbac815abbcf351ae93c43a8cd65070bd4b795f52e7
+size 5775
--- screens/ForYouScreen.js
+++ screens/ForYouScreen.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6f6060d48350f7e1e76ab2311953329c916566931c210bf6e3b73a9d768840a
-size 5662
+oid sha256:559f5412906749d86f2d5a2ba95e16bdd2deb5bdd1524e857d731c8b67b9aade
+size 5652
